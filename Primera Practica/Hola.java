4c3db652235aab7e4e5d6209a4aaedf016385455
Rafaelowsky <90066300+Rafaelowsky@users.noreply.github.com>
2022-03-17 18:44:28 -0600

    Añadí comentarios

--- Primera Practica/Hola.java
+++ Primera Practica/Hola.java
@@ -2,6 +2,7 @@
 public class Hola{
   // El metodo main es el que inicializa todo programa en Java
   public static void main(String args[]){
-   System.out.println("Hola Mundo");
+    //El system.out.println imprime en la pantalla el mensaje que fues escrito entre comillas y los parentesis
+    System.out.println("Hola Mundo");
  }
 }
