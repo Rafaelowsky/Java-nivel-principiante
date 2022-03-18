295613742dcc97b8ddfe7032adf5a05fee26183b
Rafaelowsky <90066300+Rafaelowsky@users.noreply.github.com>
2022-03-17 18:21:17 -0600

    Añadí Suma.java

---
+++ Segunda Practica/Suma.java
@@ -0,0 +1,13 @@
+public class Suma{
+   public static void main(String args[]){
+    
+    //En este paso inicializamos nuestras variables
+    int numero_uno = 5;
+    int numero_dos = 2;
+    int resultado = 0;
+
+    //Este paso hace la suma entre ambas variables y la aloja en resultado
+    resultado = numero_uno + numero_dos;
+    System.out.println("El valor de tu suma es: " + resultado);
+ }
+}
