50256717d80ea0f2bff52b8f70690c8d2fae082e
Rafaelowsky <90066300+Rafaelowsky@users.noreply.github.com>
2022-03-17 18:23:03 -0600

    Añadi comentarios

--- Primera Practica/Hola.java
+++ Primera Practica/Hola.java
@@ -1,6 +1,6 @@
-// Este es mi primer codigo en Java
-
+//El nombre de las clases siempre tiene que iniciar con mayusculas
 public class Hola{
+  // El metodo main es el que inicializa todo programa en Java
   public static void main(String args[]){
    System.out.println("Hola Mundo");
  }
